4ae18910d1ef70f49059fe53e9a9dd04fc10369c
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-04-27 16:37:32 -0400

    Inital commit

---
+++ GC_Lab_16.1/GC_Lab_16.1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GC_Lab_16._1.Models;
+
+namespace GC_Lab_16._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View(new User());
+        }
+
+
+        public IActionResult UserPref(User u)
+        {
+            //All validation is on the Client Side per the assignment. 
+            return View(u);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
